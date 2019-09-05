69b51d0918bde1225940f691dc7200ee6a3b6f4e
Fedde van Gils <feddevangils@gmail.com>
2019-09-05 08:58:19 +0200

    add create form

--- app/Http/Controllers/categoriesController.php
+++ app/Http/Controllers/categoriesController.php
@@ -24,7 +24,7 @@ class categoriesController extends Controller
      */
     public function create()
     {
-      
+       return view('categories/create');
     }
 
     /**
---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Create Category</h1>
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="text" name="name">
+        </div>
+
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea name="description"></textarea>
+        </div>
+
+        <div class="form-group">
+            <input type="submit" value="Categorie Aanmaken">
+        </div>
+    </form>
+</body>
+</html>
